cd5f22f9a856c34b7879ada2ebd1b8b68e9ce4d3
Alex Barker <abarke28@gmail.com>
2020-04-22 21:31:25 -0700

    parameterized some tests

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestNinja.UnitTests
+{
+    public class MathTests
+    {
+        [Fact]
+        public void Add_WhenCalled_ReturnSumOfArguments()
+        {
+            // Arrange
+            var math = new TestNinja.Fundamentals.Math();
+
+            // Act
+            var result = math.Add(1, 2);
+
+            // Assert
+            Assert.Equal(3, result);
+        }
+
+        [Theory]
+        [InlineData(1,1,1)]
+        [InlineData(1,2,2)]
+        [InlineData(2,1,2)]
+        public void Max_NumbersInEqual_ReturnLargerArgument(int value1, int value2, int expected)
+        {
+            // Arrange
+            var math = new TestNinja.Fundamentals.Math();
+
+            // Act
+            var result = math.Max(value1, value2);
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
