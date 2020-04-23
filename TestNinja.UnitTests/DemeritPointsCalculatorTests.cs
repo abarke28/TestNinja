a2ee09c325516034a290cc982e7fd5d39c4c2980
Alex Barker <abarke28@gmail.com>
2020-04-23 09:40:50 -0700

    More changes

---
+++ TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+using Xunit;
+
+namespace TestNinja.UnitTests
+{
+    public class DemeritPointsCalculatorTests
+    {
+        [Theory]
+        [InlineData(20,0)]
+        [InlineData(70,1)]
+        [InlineData(80,3)]
+        public void CalculateDemeritPoints_ValidSpeed_ProducesCorrectResponse(int speed, int expectedDemeritPoints)
+        {
+            // Arrange
+            var demeritPointsCalculator = new DemeritPointsCalculator();
+
+            // Act
+            var result = demeritPointsCalculator.CalculateDemeritPoints(speed);
+
+            // Assert
+            Assert.Equal(expectedDemeritPoints, result);
+        }
+
+        [Theory]
+        [InlineData(-10)]
+        [InlineData(310)]
+        public void CalculateDemeritPoints_InvalidSpeed_ThrowsArgumentOutOfRangeException(int speed)
+        {
+            // Arrange
+            var demeritPointsCalculator = new DemeritPointsCalculator();
+
+            // Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(()=>demeritPointsCalculator.CalculateDemeritPoints(speed));
+        }
+    }
+}
---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+using Xunit;
+
+namespace TestNinja.UnitTests
+{
+    public class FizzBuzzTests
+    {
+        [Theory]
+        [InlineData(3,"Fizz")]
+        [InlineData(5,"Buzz")]
+        [InlineData(15,"FizzBuzz")]
+        [InlineData(4,"4")]
+        public void GetOutput_WhenCalled_ReturnsCorrectOutput(int input, string expected)
+        {
+            // Arrange & Act
+            var result = FizzBuzz.GetOutput(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
