f03ce87401c0793c9353d03c844a6792ff8e6b21
Alex Barker <abarke28@gmail.com>
2020-04-23 12:47:18 -0700

    tests for OrderServices

---
+++ TestNinja.UnitTests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+using Xunit;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    public class OrderServiceTests
+    {
+        [Fact]
+        public void PlaceOrder_WhenCalled_StoresOrder()
+        {
+            // Arrange
+            var storage = new Mock<IStorage>();
+            var orderService = new OrderService(storage.Object);
+            var order = new Order();
+
+            // Act
+            orderService.PlaceOrder(order);
+
+            // Assert
+            storage.Verify(s => s.Store(order));
+        }
+    }
+}
