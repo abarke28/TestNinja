8e4c726362a8e6a8121434b9f6538e2b5d0fa774
Alex Barker <abarke28@gmail.com>
2020-04-23 11:44:01 -0700

    DI refactoring

---
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+using Xunit;
+
+namespace TestNinja.UnitTests
+{
+    public class VideoServiceTests
+    {
+        [Fact]
+        public void GetVideoTitle_EmptyFile_ReturnsError()
+        {
+            // Arrange
+            var fileReader = new Mock<IFileReader>();
+            fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
+
+            var videoService = new VideoService(fileReader.Object);
+
+            // Act
+            var result = videoService.ReadVideoTitle();
+
+            Assert.Contains("Error", result);
+        }
+    }
+}
